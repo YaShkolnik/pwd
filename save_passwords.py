86150ad2998db974a2161dd530d2a87ce068a5d1
Onur Galandarov <galandarovonur@yandex.ru>
2023-07-01 14:40:26 +0300

    1st version

---
+++ save_passwords.py
@@ -0,0 +1,21 @@
+def req_pwd():
+    web = input('Website name: ')
+    passwd = input('Password: ')
+    with open('passwords.log', 'a') as log:
+        print(web+':', passwd, file=log)
+
+
+def read_pwd():
+    with open('passwords.log') as log:
+        cont = log.read()
+    return cont
+
+
+while True:
+    command = input()
+    if command == 'save_pwd':
+        req_pwd()
+    elif command == 'show_pwds':
+        print(read_pwd())
+    else:
+        print('Command not found.')
